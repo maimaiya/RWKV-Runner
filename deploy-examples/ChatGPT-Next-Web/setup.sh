592f469e5084d484f69d8dc4d222dc236b51cec5
Li Xiao L <53231332+maimaiya@users.noreply.github.com>
2023-08-05 14:56:46 +0800

    Update setup.sh
    sudo apt-get -y

--- deploy-examples/ChatGPT-Next-Web/setup.sh
+++ deploy-examples/ChatGPT-Next-Web/setup.sh
@@ -1,27 +1,27 @@
 # install git python3.10 yarn by yourself
 # change model and strategy according to your hardware
 
-sudo apt-get -y install python-dev
+sudo apt-get -y install python3-dev
 
-# mkdir RWKV-Next-Web
-# cd RWKV-Next-Web
+mkdir RWKV-Next-Web
+cd RWKV-Next-Web
 
-# git clone https://github.com/josStorer/RWKV-Runner --depth=1
-python -m pip install torch torchvision torchaudio
-python -m pip install -r /kaggle/working/RWKV-Runner/backend-python/requirements.txt
-python /kaggle/working/RWKV-Runner/backend-python/main.py > log.txt &
+git clone https://github.com/josStorer/RWKV-Runner --depth=1
+python3 -m pip install torch torchvision torchaudio
+python3 -m pip install -r RWKV-Runner/backend-python/requirements.txt
+python3 ./RWKV-Runner/backend-python/main.py > log.txt &
 
-if [ ! -d /kaggle/working/RWKV-Runner/models ]; then
-    mkdir /kaggle/working/RWKV-Runner/models
+if [ ! -d RWKV-Runner/models ]; then
+    mkdir RWKV-Runner/models
 fi
-wget -N https://huggingface.co/BlinkDL/rwkv-4-world/resolve/main/RWKV-4-World-0.1B-v1-20230520-ctx4096.pth -P /kaggle/working/RWKV-Runner/models/
+wget -N https://huggingface.co/BlinkDL/rwkv-4-world/resolve/main/RWKV-4-World-0.1B-v1-20230520-ctx4096.pth -P RWKV-Runner/models/
 
 git clone https://github.com/Yidadaa/ChatGPT-Next-Web --depth=1
-cd /kaggle/working/ChatGPT-Next-Web
+cd ChatGPT-Next-Web
 yarn install
 yarn build
 export PROXY_URL=""
 export BASE_URL=http://127.0.0.1:8000
 yarn start &
 
-curl http://127.0.0.1:8000/switch-model -X POST -H "Content-Type: application/json" -d '{"model":"/kaggle/working/RWKV-Runner/models/RWKV-4-World-0.1B-v1-20230520-ctx4096.pth","strategy":"cpu fp32"}'
+curl http://127.0.0.1:8000/switch-model -X POST -H "Content-Type: application/json" -d '{"model":"./RWKV-Runner/models/RWKV-4-World-0.1B-v1-20230520-ctx4096.pth","strategy":"cpu fp32"}'
